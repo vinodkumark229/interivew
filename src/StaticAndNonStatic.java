b018503819c37efd806f010c7aa08789180d2f58
vinodkumark229 <89702884+vinodkumark229@users.noreply.github.com>
2022-05-05 09:20:16 -0400

    First commit

---
+++ src/StaticAndNonStatic.java
@@ -0,0 +1,15 @@
+
+public class StaticAndNonStatic {
+	
+	String name = "vinod";
+	//int age = 30;
+	static int age = 30; // made this static
+	
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
