966a460841df519a1803eda52c80580c8f942ae6
vinodkumark229 <89702884+vinodkumark229@users.noreply.github.com>
2022-05-05 09:51:43 -0400

    update 3

---
+++ src/qustiona/GitStudy.java
@@ -0,0 +1,14 @@
+package qustiona;
+
+public class GitStudy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 10;
+int y =20;
+int c = x+y;
+System.out.println(c);
+
+	}
+
+}
