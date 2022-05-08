e9fc9d144fd820662fb03bff8a40b5482e6544b6
vinodkumark229 <89702884+vinodkumark229@users.noreply.github.com>
2022-05-07 22:31:07 -0400

    gi

---
+++ src/qustiona/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+package qustiona;
+
+public class AddTwoNumbers {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x= 20;
+		int y = 30;
+		int c= x+y;
+		System.out.println(c);
+	}
+
+}
